899e9457a9e8f1de92fe3f5dd67080d2f56fd4dd
Matt Bierner <12821956+mjbvz@users.noreply.github.com>
2025-11-12 10:07:40 -0800

    Update calls to preLaunch scripts

--- build/lib/preLaunch.ts
+++ build/lib/preLaunch.ts
@@ -2,9 +2,6 @@
  *  Copyright (c) Microsoft Corporation. All rights reserved.
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
-
-// @ts-check
-
 import path from 'path';
 import { spawn } from 'child_process';
 import { promises as fs } from 'fs';
